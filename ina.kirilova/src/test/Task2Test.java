eaeeea377761ed943c05af0e423a25e356fe03af
Ina <inasvetoslavova@abv.bg>
2022-09-09 13:26:48 +0300

    tests for task2

--- ina.kirilova/src/test/Task2Test.java
+++ ina.kirilova/src/test/Task2Test.java
@@ -8,10 +8,16 @@ class Task2Test {
 
     @Test
     void compare() {
+        double side1 = 4;
+        double side2 = 3;
+        String message = "first is bigger";
+        assertEquals(message, compare(side1,side2));
     }
 
     @Test
     void contcomp() {
+        String answer = "yes";
+        assertEquals("yes", answer);
     }
 
     @Test
