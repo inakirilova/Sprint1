0ee9007a590ae7d0bc545f0ad11ca93e797981e0
Ina <inasvetoslavova@abv.bg>
2022-09-09 12:06:14 +0300

    test2

---
+++ ina.kirilova/src/Task2Test.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task2Test {
+
+    @Test
+    void compare() {
+    }
+
+    @Test
+    void contcomp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
