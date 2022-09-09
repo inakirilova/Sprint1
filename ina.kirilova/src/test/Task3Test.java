e6ad139ee5b31f0c2e881585bd22309df534349f
Ina <inasvetoslavova@abv.bg>
2022-09-09 14:23:11 +0300

    tests for task3

---
+++ ina.kirilova/src/test/Task3Test.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task3Test {
+
+    @Test
+    void valueSort() {
+    }
+
+    @Test
+    void getarea() {
+    }
+
+    @Test
+    void cont() {
+    }
+
+    @Test
+    void main() {
+    }
+}
