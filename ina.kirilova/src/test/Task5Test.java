4e18ec99e1acb8749539e2e8027c7295425be3c3
Ina <inasvetoslavova@abv.bg>
2022-09-11 21:55:38 +0300

    Tests implementation. Code refactored

--- ina.kirilova/src/test/Task5Test.java
+++ ina.kirilova/src/test/Task5Test.java
@@ -14,11 +14,17 @@ public class Task5Test {
             "Twenty One", "Twenty Two", "Twenty Three", "Twenty Four", "Twenty Five", "Twenty Six", "Twenty Seven", "Twenty Eight", "Twenty Nine",
             "Thirty ","Thirty One", "Thirty Two", "Thirty Three", "Thirty Four", "Thirty Five", "Thirty Six", "Thirty Seven", "Thirty Eight", "Thirty Nine",
             "Forty ","Forty One", "Forty Two", "Forty Three", "Forty Four", "Forty Five", "Forty Six", "Forty Seven", "Forty Eight", "Forty Nine",
+            "Fifty ","Fifty One", "Fifty Two", "Fifty Three", "Fifty Four", "Fifty Five", "Fifty Six", "Fifty Seven", "Fifty Eight", "Fifty Nine",
+            "Sixty ","Sixty One", "Sixty Two", "Sixty Three", "Sixty Four", "Sixty Five", "Sixty Six", "Sixty Seven", "Sixty Eight", "Sixty Nine",
+            "Seventy ","Seventy One", "Seventy Two", "Seventy Three", "Seventy Four", "Seventy Five", "Seventy Six", "Seventy Seven", "Seventy Eight", "Seventy Nine",
+            "Eighty ","Eighty One", "Eighty Two", "Eighty Three", "Eighty Four", "Eighty Five", "Eighty Six", "Eighty Seven", "Eighty Eight", "Eighty Nine",
+            "Ninety ","Ninety One", "Ninety Two", "Ninety Three", "Ninety Four", "Ninety Five", "Ninety Six", "Ninety Seven", "Ninety Eight", "Ninety Nine",
+
     };
 
     @Test
     public void test() {
-        for (int i = 1; i <= 49; i++) {
+        for (int i = 1; i <= 99; i++) {
             String number = Task5.numberConverters(String.valueOf(i));
             Assertions.assertEquals(numbers[i].trim(), number.trim());
         }
